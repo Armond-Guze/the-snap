e1890b56e0c98df2bf55e35f8e8fe79fd3d4a339
Armond-Guze <Armond.Guze@yahoo.com>
2025-06-28 15:20:39 -0400

    added footer, nav

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import Headlines from "./components/Headlines";
 import BentoGrid from "./components/BentoGrid";
 import TrendingTopics from "./components/TrendingTopics";
 import NewsletterSignup from "./components/NewsletterSignup";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Headlines />
       <BentoGrid />
       <Cards />
       <FeaturesSection />
       <TrendingTopics />
       <NewsletterSignup />
+      <Footer />
     </main>
   );
 }
