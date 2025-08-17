6fdbf75742ead6657c5959349e1ecb00a7e676f2
Armond-Guze <Armond.Guze@yahoo.com>
2025-08-17 13:49:32 -0400

    efefr

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import FantasySection from "./components/FantasySection";
 import MoreHeadlinesSection from "./components/MoreHeadlinesSection";
 // import TrendingTopics from "./components/TrendingTopics";
 import GameSchedule from "./components/GameSchedule";
-// import GoogleAds from "./components/GoogleAds";
+import GoogleAds from "./components/GoogleAds";
 import { client } from "@/sanity/lib/client";
 import { featuredGamesQuery } from "@/sanity/lib/queries";
 import { Metadata } from 'next'
@@ -41,7 +41,7 @@ export default async function Home() {
   return (
     <main className="min-h-screen">
   <GameSchedule games={featuredGames || []} />
-  {/* <GoogleAds /> */}
+  <GoogleAds />
   <Headlines textureSrc="/images/texture-image.jpg" hideSummaries />
   <RankingsSection textureSrc="/images/texture-image.jpg" hideSummaries />
   <FantasySection textureSrc="/images/texture-image.jpg" hideSummaries />
